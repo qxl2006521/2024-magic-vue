4a4b35161b31aa9655043b5cd062911177851701
qxl2006521 <qxl2006521@qq.com>
2024-03-09 17:28:34 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,202 @@
+<script setup>
+import { shallowRef, ref, computed, onMounted } from 'vue'
+import { ArrowLeft, ArrowRight } from '@element-plus/icons-vue'
+import { useResizeObserver } from '@vueuse/core'
+import * as allSteps from './steps'
+import * as _ from 'lodash-es'
+
+// 实际的牌, 主要操作这个
+const realCards = shallowRef({ row1: [], row2: [], hasGap: 0, title: '', option: 0 })
+// 主界面宽度,为了适配界面
+const containerWidth = ref(1200)
+// 为了有动画效果,需要将实际牌的两个数组全并为一个.
+const renderCards = computed(() => {
+  const result = []
+  // 图片宽度250
+  const row1Count = realCards.value.row1.length
+  const gaptotal = containerWidth.value - 20 - row1Count * 250 
+  let gap = 0
+  if( row1Count>1 )
+    gap = gaptotal / (row1Count - 1)
+  for (let i = 0; i < row1Count; i++) {
+    result.push({
+      ...realCards.value.row1[i],
+      left: i * 250 + 10 + gap * i,
+      top: 0,
+      zIndex: i
+    })
+  }
+  for (let i = 0; i < realCards.value.row2.length; i++) {
+    result.push({
+      ...realCards.value.row2[i],
+      left:i * 250 + 10 + gap * i,
+      top: 182 + realCards.value.hasGap * 80,
+      zIndex: i
+    })
+  }
+  return result
+})
+// 所有的操作步骤.可以自由组合.
+const steps = [
+  new allSteps.Step10(),
+  new allSteps.Step20(),
+  new allSteps.Step30(),
+  new allSteps.Step40(),
+  new allSteps.Step50(),
+  new allSteps.Step60(),
+  new allSteps.Step70(),
+  new allSteps.Step80(),
+  new allSteps.Step90(),
+  new allSteps.Step100(),
+  new allSteps.Step110(),
+  new allSteps.Step120()
+]
+// 初始化,先进行第一步.
+onMounted(() => {
+  containerWidth.value = document.querySelector('.container').clientWidth
+  realCards.value = steps[0].play()
+})
+
+const dialogShow = ref(false)
+const curStepIndex = ref(0)
+const btnOptions = shallowRef({ title: '', btns: [] })
+
+const _runNextStep = () => {
+  const result = steps[curStepIndex.value].play(realCards.value)
+  if (_.isArray(result)) {
+    let i = 0
+    const timeid = setInterval(() => {
+      realCards.value = result[i]
+      i++
+      if (i == result.length) {
+        clearInterval(timeid)
+      }
+    }, 500)
+  } else {
+    realCards.value = result
+  }
+}
+const preStep = () => {
+  if (curStepIndex.value <= 0) return
+  curStepIndex.value = curStepIndex.value - 1
+  realCards.value = steps[curStepIndex.value].getLastResult()
+}
+const nextStep = () => {
+  if (curStepIndex.value == steps.length - 1) return
+  curStepIndex.value = curStepIndex.value + 1
+  btnOptions.value = steps[curStepIndex.value].getSetting()
+  if (btnOptions.value) {
+    // 需要交互
+    dialogShow.value = true
+  } else {
+    // 不需要交互
+    _runNextStep()
+  }
+}
+const dialogNext = (option) => {
+  realCards.value.option = option
+  _runNextStep()
+  dialogShow.value = false
+}
+const getAssetsFile = (url) => {
+  return new URL(`./assets/${url}`, import.meta.url).href
+}
+// 监听元素尺寸,调整界面
+const el = ref(null);
+useResizeObserver(el, (entries) => {
+  const entry = entries[0]
+  const { width } = entry.contentRect
+  containerWidth.value = width
+})
+</script>
+
+<template>
+  <div class="header">
+    <div>
+      <el-button @click="preStep()" type="primary" :icon="ArrowLeft">上一步</el-button>
+       <text class="stepText">{{ curStepIndex + 1 }} / {{ steps.length }}</text>
+      <el-button @click="nextStep()" type="primary"
+        >下一步<el-icon class="el-icon--right"><ArrowRight /></el-icon
+      ></el-button>
+    </div>
+    <div class="title">
+      {{ realCards.title }}
+    </div>
+  </div>
+  <TransitionGroup name="list" tag="div" class="container" ref="el">
+    <div
+      v-for="card in renderCards"
+      class="card"
+      :key="card.id + card.part"
+      :style="{
+        left: card.left + 'px',
+        top: card.top + 'px',
+        zIndex: card.zIndex,
+        backgroundImage: 'url(' + getAssetsFile(card.id) + ')',
+        backgroundPosition: card.part == 'up' ? '0 0' : '0 -182px'
+      }"
+    ></div>
+  </TransitionGroup>
+  <el-dialog v-model="dialogShow" :title="btnOptions.title" width="600">
+    <el-button v-for="btn in btnOptions.btns" :key="btn.text" @click="dialogNext(btn.option)">{{
+      btn.text
+    }}</el-button>
+  </el-dialog>
+</template>
+
+<style scoped>
+.header {
+  width: 100%;
+  height: 15vh;
+  position: relative;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  margin: 50px auto;
+  align-items: center;
+}
+.title {
+  text-align: center;
+  font-size: 30px;
+  font-weight: bold;
+  margin-top:15px;
+}
+.stepText {
+  margin:5px 20px;
+  font-size: 20px;
+}
+.container {
+  margin: 0 auto;
+  width: 100%;
+  height: 80vh;
+  position: relative;
+}
+.card {
+  /* border-radius: 5px; */
+  position: absolute;
+  width: 250px;
+  height: 182px;
+  background-size: 250px 363px;
+  background-repeat: no-repeat;
+}
+.rotate180 {
+  transform: rotate(180deg);
+}
+.list-move, /* 对移动中的元素应用的过渡 */
+.list-enter-active,
+.list-leave-active {
+  transition: all 0.5s ease;
+}
+
+.list-enter-from,
+.list-leave-to {
+  opacity: 0;
+  transform: translateY(-60px);
+}
+
+/* 确保将离开的元素从布局流中删除
+  以便能够正确地计算移动的动画。 */
+.list-leave-active {
+  position: absolute;
+}
+</style>
---
+++ src/steps.js
@@ -0,0 +1,282 @@
+import * as _ from 'lodash-es'
+import { allCards } from './allCards' //500*726 //250*363  //250*182
+
+// 基类,提供共同的一些函数
+class StepBase {
+  constructor() {
+    this._lastResult = {}
+  }
+
+  getLastResult() {
+    return this._lastResult
+  }
+
+  getSetting() {
+    return false
+  }
+
+  play(realCards) {
+    let cards = _.cloneDeep(realCards)
+    cards = this._play(cards)
+    if (_.isArray(cards)) {
+      this._lastResult = _.cloneDeep(cards.at(-1))
+    } else {
+      this._lastResult = _.cloneDeep(cards)
+    }
+    return cards
+  }
+}
+
+// 卡牌初始化.随机选择四张牌
+export class Step10 extends StepBase {
+  constructor() {
+    super()
+  }
+  _play() {
+    const cards = _.sampleSize(allCards, 4)
+    const row1 = cards.map((item) => {
+      return {
+        id: item,
+        part: 'up'
+      }
+    })
+    const row2 = cards.map((item) => {
+      return {
+        id: item,
+        part: 'down'
+      }
+    })
+    return { row1, row2, hasGap: 0, title: '随机选择四张牌[刷新换牌]' }
+  }
+}
+
+// 洗牌
+export class Step20 extends StepBase {
+  constructor() {
+    super()
+  }
+  _play(realCards) {
+    const cards = _.shuffle(_.shuffle(realCards.row1))
+    const row1 = cards.map((item) => {
+      return {
+        id: item.id,
+        part: 'up'
+      }
+    })
+    const row2 = cards.map((item) => {
+      return {
+        id: item.id,
+        part: 'down'
+      }
+    })
+    return { row1, row2, hasGap: 0, title: '洗牌' }
+  }
+}
+
+// 撕牌
+export class Step30 extends StepBase {
+  constructor() {
+    super()
+  }
+  _play(realCards) {
+    realCards.hasGap = 1
+    realCards.title = '撕牌'
+    return realCards
+  }
+}
+
+// 变为一排
+export class Step40 extends StepBase {
+  constructor() {
+    super()
+  }
+  _play(cards) {
+    cards.row1 = [...cards.row1, ...cards.row2]
+    cards.row2 = []
+    cards.title = '将两排全并为一排'
+    return cards
+  }
+}
+
+// 根据名字将牌放到最后,需要交互
+export class Step50 extends StepBase {
+  constructor() {
+    super()
+  }
+  getSetting() {
+    return {
+      title: '请选择你名字的字数',
+      btns: [
+        {
+          text: '2个字',
+          option: 2
+        },
+        {
+          text: '3个字',
+          option: 3
+        },
+        {
+          text: '4个字',
+          option: 4
+        },
+        {
+          text: '5个字',
+          option: 5
+        },
+        {
+          text: '6个字',
+          option: 6
+        },
+        {
+          text: '7个字',
+          option: 7
+        }
+      ]
+    }
+  }
+
+  _play(cards) {
+    const num = cards.option || 3
+    const result = []
+    for (let i = 0; i < num; i++) {
+      cards.row1 = [...cards.row1.slice(1), cards.row1[0]]
+      cards.title = '根据名字字数将前面几张牌放到最后'
+      result.push(_.cloneDeep(cards))
+    }
+    return result
+  }
+}
+
+// 将最前面的三张插到中间
+export class Step60 extends StepBase {
+  constructor() {
+    super()
+  }
+  _play(cards) {
+    // [1,2,3,4,5,6,7,8] , [4,5,6,7,8] 共四个空,随机选择一个空位
+    const before3Cards = cards.row1.slice(0, 3)
+    const InserIndex = _.sample([4, 5, 6, 7])
+    const leftCards = cards.row1.slice(3, InserIndex)
+    const rightCards = cards.row1.slice(InserIndex)
+    cards.row1 = [...leftCards, ...before3Cards, ...rightCards]
+    cards.title = '将最前面的三张插到中间'
+    return cards
+  }
+}
+
+// 最上面的一张牌藏起来
+export class Step70 extends StepBase {
+  _play(cards) {
+    cards.row2 = cards.row1.slice(0, 1)
+    cards.row1 = cards.row1.slice(1)
+    cards.title = '最上面的一张牌藏起来'
+    return cards
+  }
+}
+
+// 根据[南方人,北方人,不清楚] 将牌插入中间.需要交互
+export class Step80 extends StepBase {
+  getSetting() {
+    return {
+      title: '请选择你的地域',
+      btns: [
+        {
+          text: '南方人',
+          option: 1
+        },
+        {
+          text: '北方人',
+          option: 2
+        },
+        {
+          text: '不清楚',
+          option: 3
+        }
+      ]
+    }
+  }
+  _play(cards) {
+    const num = cards.option || 2
+    // 现在第一行还有7张牌 [0,1,2,3,4,5,6]
+    const beforeCards = cards.row1.slice(0, num)
+    const InserIndex = _.random(num + 1, 6)
+    const leftCards = cards.row1.slice(num, InserIndex)
+    const rightCards = cards.row1.slice(InserIndex)
+    cards.row1 = [...leftCards, ...beforeCards, ...rightCards]
+    cards.title = '根据地域将牌插入中间'
+    return cards
+  }
+}
+
+// 需要[男, 女], 将最上面的牌扔掉,需要交互
+export class Step90 extends StepBase {
+  getSetting() {
+    return {
+      title: '请选择你的性别',
+      btns: [
+        {
+          text: '男性',
+          option: 1
+        },
+        {
+          text: '女性',
+          option: 2
+        }
+      ]
+    }
+  }
+  _play(cards) {
+    const num = cards.option || 1
+    cards.row1 = cards.row1.slice(num)
+    cards.title = '根据性别将最上面的牌扔掉'
+    return cards
+  }
+}
+
+// 见证奇迹的时刻, 每一次都将最上面的放到最下面
+export class Step100 extends StepBase {
+  _play(cards) {
+    const result = []
+    const title = '见证奇迹的时刻'
+    for (let i = 0; i < 7; i++) {
+      cards.row1 = [...cards.row1.slice(1), cards.row1[0]]
+      cards.title = '见证奇迹的时刻====' + title[i]
+      result.push(_.cloneDeep(cards))
+    }
+    return result
+  }
+}
+
+// 好运留下来,烦恼丢出去
+export class Step110 extends StepBase {
+  _play(cards) {
+    const result = []
+    function oneCircle() {
+      cards.row1 = [...cards.row1.slice(1), cards.row1[0]]
+      cards.title = '好运留下来!'
+      result.push(_.cloneDeep(cards))
+      cards.row1 = cards.row1.slice(1)
+      cards.title = '烦恼丢出去...'
+      result.push(_.cloneDeep(cards))
+      if (cards.row1.length > 1) {
+        oneCircle()
+      }
+    }
+    oneCircle()
+    return result
+  }
+}
+
+// 最后合并
+export class Step120 extends StepBase {
+  _play(cards) {
+    cards.hasGap = 0
+    cards.title = '完美!!!'
+    // bugfix, 最后的时候有可能下半部分在第一行,上半部分在第二行
+    if (cards.row1[0].part == 'down') {
+        // 需要换顺序
+        [ cards.row1, cards.row2 ] = [ cards.row2, cards.row1 ]
+    }
+    return cards
+  }
+}
